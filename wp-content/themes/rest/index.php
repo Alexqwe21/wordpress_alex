4e12bf714178df82a209cac92b7eef93a938fa75
alex sandro <alexsandropimenta16@gmail.com>
2025-04-26 09:54:31 -0300

    alex sandro

--- wp-content/themes/rest/index.php
+++ wp-content/themes/rest/index.php
@@ -1,6 +1,19 @@
 <?php get_header(); ?>
 
-	
 
-	<?php get_footer(); ?>
-	
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+
+
+		<h1><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+
+	<?php endwhile;
+else: ?>
+	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+<p>aqui n tem o loop</p>
+
+<?php get_footer(); ?>
