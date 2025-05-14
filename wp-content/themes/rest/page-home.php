e820f99dc4a52522c09c09f4724900d0a08dc070
alex sandro <alexsandropimenta16@gmail.com>
2025-05-14 18:36:29 -0300

    laço de repetição com CMS

--- wp-content/themes/rest/page-home.php
+++ wp-content/themes/rest/page-home.php
@@ -10,54 +10,48 @@
 			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="menu-item grid-8">
-			<?php if(have_rows('conteudo_sobre_peixe')): while(have_rows('conteudo_sobre_peixe')) : the_row(); ?>
-				<h2><?php the_sub_field('titulo_do_produto'); ?></h2>
+				<h2><?php the_field('titulo_do_produto'); ?></h2>
 				<ul>
-					<li>
-						<span><sup>R$</sup><?php the_sub_field('preco_do_produto'); ?></span>
-						<div>
-						
-							<h3><?php the_sub_field('nome_produto'); ?></h3>
-							<p><?php the_sub_field('descricao_produto'); ?></p>
-						</div>
-					</li>
-					<?php endwhile; else : endif; ?>
-					
-						<span><sup>R$</sup>159</span>
-						<div>
-							<h3>Camarão com Catupiry</h3>
-							<p>Grandes camarões grelhados, servidos ao molho de camarão com catupiry</p>
-						</div>
-					</li>
+				<?php if (have_rows('conteudo_sobre_peixe')): while (have_rows('conteudo_sobre_peixe')) : the_row(); ?>
+
+
+						<li>
+							<span><sup>R$</sup><?php the_sub_field('preco_do-produto'); ?></span>
+							<div>
+
+								<h3><?php the_sub_field('nome_produto'); ?></h3>
+								<?php the_sub_field('descricao_produto'); ?>
+							</div>
+						</li>
+				<?php endwhile;
+				else : endif; ?>
+
+
+
 				</ul>
-				
+
 			</div>
 
 			<div class="menu-item grid-8">
-				<h2>Carnes</h2>
-				<ul>
-					<li>
-						<span><sup>R$</sup>129</span>
-						<div>
-							<h3>Picanha Nobre no Alho</h3>
-							<p>Pequenas tiras de salmão feitas no alho e óleo</p>
-						</div>
-					</li>
-					<li>
-						<span><sup>R$</sup>89</span>
-						<div>
-							<h3>Cupim no Bafo</h3>
-							<p>Sardinhas escolhidas a dedo e fritas em cerveja premium</p>
-						</div>
-					</li>
-					<li>
-						<span><sup>R$</sup>159</span>
-						<div>
-							<h3>Hamburger Artesanal Rest</h3>
-							<p>Grandes camarões grelhados, servidos ao molho de camarão com catupiry</p>
-						</div>
-					</li>
+				<h2><?php the_field('titulo_do_produto_carnes'); ?></h2>
+				<ul></ul>
+				<?php if (have_rows('conteudo_sobre_carnes')): while (have_rows('conteudo_sobre_carnes')) : the_row(); ?>
+
+						<li>
+							<span><sup>R$</sup><?php the_sub_field('preco_do-produto'); ?></span>
+							<div>
+
+								<h3><?php the_sub_field('nome_produto'); ?></h3>
+								<?php the_sub_field('descricao_produto'); ?>
+							</div>
+						</li>
+				<?php endwhile;
+				else : endif; ?>
+
+
+
 				</ul>
+
 			</div>
 
 		</section>
